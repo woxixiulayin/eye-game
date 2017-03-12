104f5549d547b70e7f8001b47e38b1fd22d7b0a7
Jackson Liu <416883813@qq.com>
2017-03-12 19:20:57 +0800

    resize

--- src/js/main-content/live-content/live-section/index.vue
+++ src/js/main-content/live-content/live-section/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="live-section">
+  <div class="live-section" ref='liveSection'>
     <el-tabs v-for='categoryItem in categoryList' v-show='currentCategory===categoryItem' @tab-click="handleClick" :value='currentSiteName'
       :key='categoryItem'>
-      <el-tab-pane v-for='(site,index) in categorySites[currentCategory]' :label="site" :name="site" :key='site'>用户管理
+      <el-tab-pane v-for='(site,index) in categorySites[currentCategory]' :label="site" :name="site" :key='site'>
+        <div class="video-list">
+          <video-item v-for='item in 10' :key='item' />
+        </div>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -10,6 +13,7 @@
 
 <script>
   import store from 'js/store'
+  import videoItem from './video'
 
   export default {
     props: ['currentCategory'],
@@ -28,6 +32,26 @@
         return this.categorySites[currentCategory][this.currentSiteIndexMap.get(currentCategory)]
       }
     },
+    components: {
+      videoItem
+    },
+    mounted () {
+      const liveSection = this.$refs.liveSection
+      console.log(liveSection)
+      // const that = this
+      const reSizeIfWidthChange = (() => {
+        let lastWidth = liveSection.clientWidth
+        return () => {
+          let width = liveSection.clientWidth
+          if (width !== lastWidth) {
+            lastWidth = width
+            console.log(width)
+          }
+        }
+      })()
+
+      setInterval(reSizeIfWidthChange, 500)
+    },
     methods: {
       handleClick: () => {}
     }
@@ -35,10 +59,22 @@
 
 </script>
 
-<style>
+<style lang='scss' scoped>
   .live-section {
     flex-grow: 1;
     flex-shrink: 1;
+  }
+  
+  .video-list {
+    display: inline-flex;
+    flex-wrap: wrap;
+    flex-grow: 0;
+    justify-content: center;
+    padding: 10px 20px;
+  }
+  
+  .video-item-wrap {
+    display: flex;
   }
 
 </style>
---
+++ src/js/main-content/live-content/live-section/video.vue
@@ -0,0 +1,83 @@
+<template>
+  <li class="live-item">
+    <div class="video-cover">
+      <img :src='img' alt="" class="video-img" />
+      <div class="play-mask"></div>
+    </div>
+    <div class="video-title">{{title}}</div>
+    <div class="video-info">
+      <span class="video-nickname">{{name}}</span>
+      <span class="video-nums">{{nums}}</span>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['video'],
+    data: () => {
+      return {
+        name: '灰灰',
+        nums: '629',
+        title: '小奶：单排冲8000！         ' +
+          '               ',
+        link: 'http://www.douyu.com/xiaonai',
+        img: 'https://rpic.douyucdn.cn/a1610/24/20/224460_161024205221.jpg'
+      }
+    }
+  }
+
+</script>
+
+<style lang='scss' scoped>
+  .live-item {
+    height: 215px;
+    flex-basis: 290px;
+    flex-grow: 1;
+    flex-shrink: 0;
+    border: 1px solid black;
+    background: white;
+    margin: 5px 5px;
+    display: flex;
+    flex-flow: row wrap;
+    .video-cover {
+      position: relative;
+      width: 100%;
+      height: 158px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .play-mask {
+        position: absolute;
+        left: 0;
+        height: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        background: #fff;
+      }
+    }
+    .video-title {
+      margin-left: 5px;
+      width: 100%;
+      line-height: 30px;
+      white-space: nowrap;
+      font-size: 15px;
+    }
+    .video-info {
+      margin-left: 5px;
+      font-size: 14px;
+      width: 100%;
+      .video-nickname {
+        margin-left: 3px;
+        float: left;
+      }
+      .video-nums {
+        float: right;
+        margin-right: 5px;
+      }
+    }
+  }
+
+</style>
